99ace26c6eea60a92e2c86a876d883c285ac5229
Leo <leo@hexafuchs.de>
2024-07-16 16:06:15 +0200

    :sparkles: Add team model and migration

--- config/teams.php
+++ config/teams.php
@@ -2,5 +2,28 @@
 
 // config for Hexafuchs/Team
 return [
+    'models' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Team Model Class
+        |--------------------------------------------------------------------------
+        |
+        | Defines the model class to use for teams. Should extend
+        | \Hexafuchs\Team\Team.
+        |
+        */
+        'team' => \Hexafuchs\Team\Team::class,
+        /*
+        |--------------------------------------------------------------------------
+        | User Model Class
+        |--------------------------------------------------------------------------
+        |
+        | Defines the model class to use for users. Typically this is your default
+        | \App\Models\User.
+        |
+        */
+        'user' => \Illuminate\Foundation\Auth\User::class,
+    ]
 
 ];
--- src/Team.php
+++ src/Team.php
@@ -2,4 +2,14 @@
 
 namespace Hexafuchs\Team;
 
-class Team {}
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Team extends Model {
+
+
+    public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(config('teams.model.user'));
+    }
+}
