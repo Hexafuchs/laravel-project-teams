2bc5f6ab1eb081fd500244fc6d043dfbf286b65f
Leo <leo@hexafuchs.de>
2024-07-21 20:02:39 +0200

    :sparkles: Add members to teams

--- src/Team.php
+++ src/Team.php
@@ -2,8 +2,11 @@
 
 namespace Hexafuchs\Team;
 
+use Hexafuchs\Team\Traits\Ownable;
+use Hexafuchs\Team\Traits\TeamMember;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User;
 
 class Team extends Model {
 
@@ -25,14 +28,70 @@ class Team extends Model {
     }
 
     /**
+     * Checks if the object has the `Hexafuchs\Team\Traits\TeamMember` trait and the class configured in
+     *  `teams.models.user` in the hierarchy.
+     */
+    protected function isTeamMemberObject(User $user): bool
+    {
+        $hasTrait = in_array(TeamMember::class, class_uses_recursive($user));
+        $isChild = is_a($user, config('teams.models.user'));
+
+        if (!app()->environment('production')) {
+            assert($hasTrait);
+            assert($isChild);
+        }
+
+        return $hasTrait && $isChild;
+    }
+
+    /**
+     * Adds a user as team member.
+     *
+     * The model needs to have the `Hexafuchs\Team\Traits\TeamMember` trait and have the class configured in
+     * `teams.models.user` in the hierarchy.
+     */
+    public function addMember(User $user)
+    {
+        if ($this->isTeamMemberObject($user)) {
+            $this->members()->attach($user);
+        }
+    }
+
+    /**
+     * Removes a user as team member.
+     *
+     * The model needs to have the `Hexafuchs\Team\Traits\TeamMember` trait and have the class configured in
+     * `teams.models.user` in the hierarchy.
+     */
+    public function removeMember(User $user)
+    {
+        if ($this->isTeamMemberObject($user)) {
+            $this->members()->detach($user);
+            $this->refresh();
+        }
+    }
+
+    /**
+     * Checks it he object has the `Hexafuchs\Team\Traits\Ownable` trait
+     */
+    protected function isOwnableObject(Model $model): bool
+    {
+        $hasTrait = in_array(Ownable::class, class_uses_recursive($model));
+        if (!app()->environment('production')) {
+            assert($hasTrait);
+        }
+        return $hasTrait;
+    }
+
+    /**
      * Checks if this team is a (co-)owner of the given model.
      *
-     * The model must be an ownable (or at least implement `isOwnedBy` with a team as first and only required
-     * parameter), else `false` is returned.
+     * The model must have the `Hexafuchs\Team\Traits\Ownable` trait.
      */
     public function owns(Model $model): bool
     {
-        if (method_exists($model, 'isOwnedBy')) {
+        if ($this->isOwnableObject($model)) {
+            assert(method_exists($model, 'isOwnedBy'));
             return $model->isOwnedBy($this);
         }
         return false;
